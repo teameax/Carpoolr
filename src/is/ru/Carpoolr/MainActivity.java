bfa1ec627daa774038a1e5862e00570b971c6806
Joddsson <ivaroddsson@gmail.com>
2014-10-11 13:56:26 +0000

    Added a splash screen with the official Carpoolr logo

--- src/is/ru/Carpoolr/MainActivity.java
+++ src/is/ru/Carpoolr/MainActivity.java
@@ -2,14 +2,19 @@ package is.ru.Carpoolr;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
     /**
      * Called when the activity is first created.
      */
+
+    Handler handler;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        handler = new Handler();
     }
 }
