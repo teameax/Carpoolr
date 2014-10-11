241a0d056747ac17a40a874e46760b2f1987570a
Joddsson <ivaroddsson@gmail.com>
2014-10-11 14:22:44 +0000

    Main activity has no header

--- src/is/ru/Carpoolr/MainActivity.java
+++ src/is/ru/Carpoolr/MainActivity.java
@@ -3,6 +3,7 @@ package is.ru.Carpoolr;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 public class MainActivity extends Activity {
     /**
@@ -13,6 +14,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.main);
 
         handler = new Handler();
