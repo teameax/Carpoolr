3ad039935ac42dc2336425a7511d062ca4a8e4bc
Ari Freyr <aria12@ru.is>
2014-10-11 13:04:18 +0000

    hello

--- src/is/ru/Carpoolr/MainActivity.java
+++ src/is/ru/Carpoolr/MainActivity.java
@@ -1,9 +1,13 @@
 package is.ru.Carpoolr;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ListActivity {
+
+    private static final String FIREBASE_URL = "https://carpoolreax.firebaseio.com/";
+    private Firebase ref;
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +15,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
     }
 }
