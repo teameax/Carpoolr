715662b62beca529d6124f681ebe82f17b09106e
Joddsson <ivaroddsson@gmail.com>
2014-10-14 19:59:46 +0000

    Customizing actionbar

--- src/is/ru/Carpoolr/MainActivity.java
+++ src/is/ru/Carpoolr/MainActivity.java
@@ -1,11 +1,20 @@
 package is.ru.Carpoolr;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import is.ru.Carpoolr.fragments.MainFragment;
 import is.ru.Carpoolr.fragments.RideListFragment;
 
@@ -13,14 +22,43 @@ import is.ru.Carpoolr.fragments.RideListFragment;
 public class MainActivity extends Activity {
     private FragmentManager fragmentManager = getFragmentManager();
     private FragmentTransaction fragmentTransaction;
+    private ActionBar actionBar;
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Add the main fragment to the fragment container and start it.
+        actionBar = getActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(false);
+            actionBar.setDisplayShowTitleEnabled(false);
+            LayoutInflater inflater = LayoutInflater.from(this);
+            View customView = inflater.inflate(R.layout.custom_actionbar, null);
+            TextView place = (TextView)customView.findViewById(R.id.header);
+            place.setText(R.string.app_name);
+            ImageButton backButton = (ImageButton) customView.findViewById(R.id.back_button);
+            backButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getApplicationContext(), "Refresh Clicked!",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
 
+            actionBar.setCustomView(customView);
+            actionBar.setDisplayShowCustomEnabled(true);
+        }
+
+
+        /*if (actionBar != null) {
+            actionBar.setBackgroundDrawable(new ColorDrawable(0xFF4fe0c0));
+            actionBar.setIcon(R.drawable.back);
+            actionBar.setTitle(R.string.app_name);
+        }*/
+
+        // Add the main fragment to the fragment container and start it.
         fragmentTransaction = fragmentManager.beginTransaction();
         MainFragment mainFragment = new MainFragment();
         fragmentTransaction.add(R.id.fragment_container, mainFragment);
