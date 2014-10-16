540f93ae60278f91039d785e0f598bbb9485ccd1
Joddsson <ivaroddsson@gmail.com>
2014-10-16 14:40:57 +0000

    Nav bar layout

--- src/is/ru/Carpoolr/MainActivity.java
+++ src/is/ru/Carpoolr/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.widget.DrawerLayout;
@@ -13,29 +14,32 @@ import android.widget.*;
 import is.ru.Carpoolr.fragments.MainFragment;
 import is.ru.Carpoolr.fragments.RideListFragment;
 
-
 public class MainActivity extends Activity {
     private FragmentManager fragmentManager = getFragmentManager();
     private FragmentTransaction fragmentTransaction;
     private ActionBar actionBar;
+    private String list_type = null;
 
+    // Variables needed for nav menu.
     private String[] mMenuListItems;
     private DrawerLayout mDrawerLayout;
     private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
     private CharSequence mDrawerTitle;
     private CharSequence mTitle;
-
     private FrameLayout frameLayout;
-
-    private String list_type = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        //linearLayout = (LinearLayout) findViewById(R.id.linearLay);
+        // Add the main fragment to the fragment container and start it.
+        MainFragment mainFragment = new MainFragment();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, mainFragment);
+        fragmentTransaction.commit();
+
         // Customizing the action bar.
         actionBar = getActionBar();
         if (actionBar != null) {
@@ -50,11 +54,11 @@ public class MainActivity extends Activity {
             actionBar.setDisplayShowCustomEnabled(true);
         }
 
+        // Navigation menu setup.
         frameLayout = (FrameLayout)findViewById(R.id.fragment_container);
         mMenuListItems = getResources().getStringArray(R.array.nav_drawer_icons);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerList = (ListView) findViewById(R.id.left_drawer);
-
 
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mMenuListItems));
         mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
@@ -62,20 +66,20 @@ public class MainActivity extends Activity {
         mTitle = mDrawerTitle = getTitle();
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.back,       /* nav drawer icon to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description */
-                R.string.drawer_close  /* "close drawer" description */
+                this,                  // host Activity
+                mDrawerLayout,         // DrawerLayout object
+                R.drawable.ic_drawer,  // nav drawer icon to replace 'Up' caret
+                R.string.drawer_open,  // "open drawer" description
+                R.string.drawer_close   // "close drawer" description
         ) {
 
-            /** Called when a drawer has settled in a completely closed state. */
+            // Called when a drawer has settled in a completely closed state.
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
                 getActionBar().setTitle(mTitle);
             }
 
-            /** Called when a drawer has settled in a completely open state. */
+            // Called when a drawer has settled in a completely open state.
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
                 getActionBar().setTitle(mDrawerTitle);
@@ -87,13 +91,7 @@ public class MainActivity extends Activity {
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
-
-
-        // Add the main fragment to the fragment container and start it.
-        MainFragment mainFragment = new MainFragment();
-        fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.add(R.id.fragment_container, mainFragment);
-        fragmentTransaction.commit();
+        getActionBar().setBackgroundDrawable(new ColorDrawable(0xFF4fe0c0)); // CYAN.
     }
 
     @Override
@@ -111,18 +109,16 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
+        // Pass the event to ActionBarDrawerToggle, if it returns true, then it has handled the app icon touch event
         if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
-        // Handle your other action bar items...
-
+        // Handle your other action bar items
         return super.onOptionsItemSelected(item);
     }
 
 
-    /* Called whenever we call invalidateOptionsMenu() */
+    // Called whenever we call invalidateOptionsMenu()
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
