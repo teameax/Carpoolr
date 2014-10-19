97dc4b2587261ad144331bdb0f02034b9eb49eae
Joddsson <ivaroddsson@gmail.com>
2014-10-19 18:31:52 +0000

    Added a home button to the successful reg view

--- src/is/ru/Carpoolr/fragments/RegistrationSuccessFragment.java
+++ src/is/ru/Carpoolr/fragments/RegistrationSuccessFragment.java
@@ -1,16 +1,19 @@
 package is.ru.Carpoolr.fragments;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import is.ru.Carpoolr.R;
 
 /**
  * Created by joddsson on 19.10.2014.
  */
 public class RegistrationSuccessFragment extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         return inflater.inflate(R.layout.registration_success, container, false);
